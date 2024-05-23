ebc62494a822d3a1cb005ed6e7695656ff2c9061
7thQ <davidoathg@gmail.com>
2024-05-22 20:23:29 -0700

    starting the logic to making the Id need for the backend managment of the locations

---
+++ generateId.js
@@ -0,0 +1,73 @@
+
+
+
+import readline from 'readline';
+
+// Readline interface for simulating button click in the console
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Object to hold the current value for each part of the ID
+const idParts = {
+  WC: 1,
+  C: 1,
+  S: 1,
+  SC: 1,
+  CT: 1,
+  N: 1,
+  UP: 1
+};
+
+// Function to generate the current unique id based on the specified structure
+const generateCurrentId = () => {
+  return `P1WC${idParts.WC}C${idParts.C}S${idParts.S}SC${idParts.SC}CT${idParts.CT}N${idParts.N}UP${idParts.UP}`;
+};
+
+// Function to increment the ID parts
+const incrementIdParts = () => {
+  // Increment the "UP" part
+  idParts.UP += 1;
+
+  // Check and carry over if needed, like an odometer
+  if (idParts.UP > 999999) {
+    idParts.UP = 1;
+    idParts.N += 1;
+  }
+  if (idParts.N > 999999) {
+    idParts.N = 1;
+    idParts.CT += 1;
+  }
+  if (idParts.CT > 999999) {
+    idParts.CT = 1;
+    idParts.SC += 1;
+  }
+  if (idParts.SC > 999999) {
+    idParts.SC = 1;
+    idParts.S += 1;
+  }
+  if (idParts.S > 999999) {
+    idParts.S = 1;
+    idParts.C += 1;
+  }
+  if (idParts.C > 999999) {
+    idParts.C = 1;
+    idParts.WC += 1;
+  }
+  // Note: WC has no further carry as we don't expect to need it based on the problem description
+};
+
+// Function to print the button and wait for a click (Enter key press)
+const printButtonAndWaitForClick = () => {
+  console.log('Press "Enter" to generate a new id.');
+  rl.question('', (answer) => {
+    const newId = generateCurrentId();
+    console.log(`Generated ID: ${newId}`);
+    incrementIdParts();  // Increment the ID after showing the current one
+    printButtonAndWaitForClick();  // Print the button again for the next id
+  });
+};
+
+// Start the process
+printButtonAndWaitForClick();
